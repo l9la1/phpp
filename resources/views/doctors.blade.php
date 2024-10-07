d5926b619a6ed37179670a4fe1d58e516eeca228
l9la1 <131354518+l9la1@users.noreply.github.com>
2024-10-07 09:57:11 +0200

    created the blade

---
+++ resources/views/doctors.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Doctor</title>
+</head>
+<body>
+    <h1>Welkom test</h1>
+
+    <table class="table table-dark table-striped">
+            <tr>
+                <th>client</th>
+                <th>datum</th>
+                <th>voor wat</th>
+                <th>addapt</th>
+                <th>remove</th>
+            </tr>
+            <tr>
+                <td>test</td>
+            </tr>
+            <tr>
+                <td>test</td>
+            </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
