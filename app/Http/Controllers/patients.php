13c40cc9dfe0eb8cee9e189ad0707117bcf1538f
R1nkol <rick.maisweg@gmail.com>
2024-10-16 01:17:16 +0200

    registration
    Ik heb een eenvoudige registratie toegevoegd, waarbij mensen automatisch in de wachtrij worden geplaatst als ze zich registreren

--- app/Http/Controllers/patients.php
+++ app/Http/Controllers/patients.php
@@ -6,10 +6,49 @@ use App\Models\financials;
 use App\Models\appointment;
 use Illuminate\Http\Request;
 
+use App\Models\Queue;
+use App\Models\patient;
+
 class patients extends Controller
 {
     public function index()
     {
         return view("patients",['financial'=>financials::where("patient_id",1)->limit(5)->get(),'appointments'=>appointment::where("patient_id",1)->orderBy("appointment_date")->get()]);
+    }
+
+    public function create()
+    {
+        return view('patientregister');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate form input
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string',
+            'phonenumber' => 'required|string',
+            'date_of_birth' => 'required|date',
+        ]);
+
+        $validatedData['registration_date'] = now();
+    
+        // Save patient data
+        $patient = patient::create($validatedData);
+    
+        // Add patient to the queue
+        queue::create([
+            'patient_id' => 1, // Set patient ID
+            'priority' => 1, // Set priority, default is 1
+            'status' => 0,   // Status: waiting
+        ]);
+    
+        return redirect()->route('patients.store');
+    }
+    
+
+    public function thankyou()
+    {
+        return view('patientregister');
     }
 }   
---
+++ app/Models/Queue.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Queue extends Model
+{
+    use HasFactory;
+
+    protected $table = 'queue';
+
+    protected $fillable = ['priority', 
+    'status', 
+    'patient_id'];
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
