a260047cb21fff09f98e6712e3b7ca3b61e285c0
R1nkol <123153700+R1nkol@users.noreply.github.com>
2024-11-04 11:58:06 +0100

    log uit added to administration

--- resources/views/administration.blade.php
+++ resources/views/administration.blade.php
@@ -35,6 +35,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/incidents" target="_blank">Incidenten</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}">Log uit</a>
+                    </li>
                 </ul>
             </div>
         </div>
